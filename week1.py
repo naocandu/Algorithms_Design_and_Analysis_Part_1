058afeda09b0d1977d294266b3384ba76851fc63
naocandu <dbianm@gmail.com>
2016-07-24 17:16:17 -0400

    Number of Inversions
    O(n*log(n))
    similar as merge sort

---
+++ week1.py
@@ -0,0 +1,46 @@
+"""
+The file "IntegerArray.txt" contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.
+Your task is to compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array.
+Because of the large size of this array, you should implement the fast divide-and-conquer algorithm.
+"""
+
+# import the array set
+# create new list
+arrayList = []
+with open('/Users/naocandu/Dropbox/Algorithm_stanford/week1/week1.txt') as f:
+    for line in f:
+        arrayList.append(line)
+# clean list
+myList = [int(item.split('\\')[0]) for item in arrayList]
+
+def merge_count_inversion(lst):
+    if len(lst) <= 1:
+        return lst, 0
+    middle = int( len(lst) / 2 )
+    left, a = merge_count_inversion(lst[:middle])
+    right, b = merge_count_inversion(lst[middle:])
+    result, c = merge_count_split_inversion(left, right)
+    return result, (a + b + c)
+
+def merge_count_split_inversion(left, right):
+    result = []
+    count = 0
+    i, j = 0, 0
+    left_len = len(left)
+    while i < left_len and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            count += left_len - i
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result, count
+
+# print number of inversions
+print merge_count_inversion(myList)[1]
+
+# we also can print the sorted list
+# print merge_count_inversion(myList)[0]
